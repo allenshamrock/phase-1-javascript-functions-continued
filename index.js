998642c957ba1230bc5250794fe349646e55b8ad
Allen <allenshamrock37@gmail.com>
2023-12-13 22:26:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(play = "roller-skate") {
+  return `This Saturday, I want to ${play}!`;
+}
+saturdayFun();
+
+const mondayWork = function (duty = "go to the office") {
+  return `This Monday, I will ${duty}.`;
+};
+
+mondayWork();
+
+function wrapAdjective(agressive = "*") {
+  return function (occassion = "special") {
+    return `You are ${agressive}${occassion}${agressive}!`;
+  };
+}
+
+wrapAdjective()()
